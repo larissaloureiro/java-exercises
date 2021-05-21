6f1456e64422a22c1c28088e108d5fbfc8c557e1
larissaloureiro <80994671+larissaloureiro@users.noreply.github.com>
2021-05-21 12:13:12 -0300

    Create Veiculo.java

---
+++ IGTI/DesafioModulo3/src/entidades/Veiculo.java
@@ -0,0 +1,61 @@
+package entidades;
+
+public abstract class Veiculo {
+	protected String modelo;
+	protected String marca;
+	protected Integer ano;
+	protected Double valor;
+	protected Vendedor vendedor;
+	protected Cliente cliente;
+	
+	
+	//Getters e Setters
+	public String getModelo() {
+		return modelo;
+	}
+	
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+	
+	public String getMarca() {
+		return marca;
+	}
+	
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	
+	public Integer getAno() {
+		return ano;
+	}
+	
+	public void setAno(Integer ano) {
+		this.ano = ano;
+	}
+	
+	public Double getValor() {
+		return valor;
+	}
+	
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+	
+	public Vendedor getVendedor() {
+		return vendedor;
+	}
+	
+	public void setVendedor(Vendedor vendedor) {
+		this.vendedor = vendedor;
+	}
+	
+	public Cliente getCliente() {
+		return cliente;
+	}
+	
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+	
+}
